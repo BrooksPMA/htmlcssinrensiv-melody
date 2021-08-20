363c603c070b14153dadbc173adea9989c479349
BrooksPMA <daniel@lukmanov.net>
2021-08-20 18:57:08 +0300

    added modal-dialog

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@ $(document).ready(function () {
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
+  var flatsPath = $(".modal-image path");
+  var flatsLink = $(".flat-link");
+
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
   });
+
+  floorPath.on("click", toggleModal);
+  modalCloseButton.on("click", toggleModal);
+  viewFlatsButton.on("click", toggleModal);
 
   counterUp.on("click", function () {
     if (currentFloor < 18) {
@@ -15,6 +25,7 @@ $(document).ready(function () {
       usNumber();
     }
   });
+
   counterDown.on("click", function () {
     if (currentFloor > 2) {
       currentFloor--;
@@ -31,4 +42,27 @@ $(document).ready(function () {
     floorPath.removeClass("current-floor");
     $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
   };
+
+  function toggleModal() {
+    modal.toggleClass("is-open");
+  }
+
+  function deleteClass() {
+    flatsPath.removeClass("current-flat");
+    flatsLink.removeClass("current-flat");
+  }
+
+  flatsPath.on("mouseover", function () {
+    deleteClass();
+    $(`[data-flat-link="${$(this).attr("data-flat")}"]`).toggleClass(
+      "current-flat"
+    );
+  });
+
+  flatsLink.on("mouseover", function () {
+    deleteClass();
+    $(`[data-flat="${$(this).attr("data-flat-link")}"]`).toggleClass(
+      "current-flat"
+    );
+  });
 });
